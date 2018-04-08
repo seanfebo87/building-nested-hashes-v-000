d26873453287d48cc0e53c9e35d6f1580076facb
Sean Febo <seanfebo@techie.com>
2018-04-08 05:35:43 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+      
+      :montague => {
       :patriarch => {}
       :matriarch => {}
       :hero => {}
       :hero_friends => [] }, 
        
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => [] }
+}
 end
